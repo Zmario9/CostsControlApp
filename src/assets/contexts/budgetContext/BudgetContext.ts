b575895548760251ec4b7db719eba2969c8c31bf
Jose Manuel <jcorreacastro99@gmail.com>
2025-03-07 15:22:45 -0400

    Esto se supone que arregla los posibles errores

--- src/assets/contexts/budgetContext/BudgetContext.ts
+++ src/assets/contexts/budgetContext/BudgetContext.ts
@@ -1,15 +1,4 @@
-import { useReducer, createContext } from "react";
-import { budgetReducer, initialState } from "../../reducers/budget-reducer";
+import { createContext } from "react";
 import { BudgetContextProps } from "./BudgetContextTypes";
 
-//Tambien puede ser "{} as BudgetContextProps"
 export const BudgetContext = createContext<BudgetContextProps>(null!);
-
-export const BudgetProvider = () => {
-    const [state, dispatch] = useReducer(budgetReducer, initialState);
-    return (
-        <BudgetContext.Provider value={{ state, dispatch }}>
-            {children}
-        </BudgetContext.Provider>
-    );
-}
