816fd00eba139b0773bdee78c6dd246c606f7c20
Jose Manuel <jcorreacastro99@gmail.com>
2025-03-10 15:38:18 -0400

    guardados antes de instalar react calendar

--- src/components/ExpenseForm/ExpenseForm.tsx
+++ src/components/ExpenseForm/ExpenseForm.tsx
@@ -1,4 +1,10 @@
 import { categories } from "../../data/categories";
+import DatePicker from "react-date-picker";
+import "react-date-picker/dist/DatePicker.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 export default function ExpenseForm() {
   return (
     <form className="space-y-5">
@@ -28,24 +34,33 @@ export default function ExpenseForm() {
           className="bg-slate-100 p-2"
           name="amount"
         />
-        <div className="flex flex-col gap-2">
-          <label htmlFor="amount" className="text-xl">
-            Categoria:
-          </label>
-          <select
-            name="category"
-            id="category"
-            className="bg-slate-100 p-2"        
-          >
-            <option value="">-- Seleccione --</option>
-            {categories.map(category =>(
-                <option value={category.id} key={category.id}>{category.name}</option>
-            ))}
-          </select>
-        </div>
       </div>
-      <input type="submit" className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-      value="Registrar Gasto"/>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Categoria:
+        </label>
+        <select name="category" id="category" className="bg-slate-100 p-2">
+          <option value="">-- Seleccione --</option>
+          {categories.map((category) => (
+            <option value={category.id} key={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecha Gasto:
+        </label>
+        <DatePicker
+          className={"bg-slate-50 p-2 border-0"}
+        />
+      </div>
+      <input
+        type="submit"
+        className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+        value="Registrar Gasto"
+      />
     </form>
   );
 }
