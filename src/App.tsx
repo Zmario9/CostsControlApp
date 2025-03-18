6f457138e04395d877bc431d45686b8a12175fae
Jose Manuel <jcorreacastro99@gmail.com>
2025-03-17 20:13:00 -0400

    guardados hasta la clase 222, donde hara un ligero cambio al godigo del state de budget reducer (en la parte ed totalextpense

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@
 import BudgetForm from "./components/BudgetForm/BudgetForm";
 import BudgetTracker from "./components/BudgetTracker/BudgetTracker";
 import { useBudget } from "./hooks/useBudget";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import ExpenseModal from "./components/ExpenseModal/ExpenseModal";
 import ExpensesList from "./components/ExpensesList/ExpensesList";
 function App() {
   const {state} = useBudget();
   const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
+
+  //Para saber cada vez que se modifique el state.
+  useEffect(() => {
+    localStorage.setItem('budget', state.budget.toString());
+    localStorage.setItem('expenses', JSON.stringify(state.expenses));
+  }, [state]);
+
   return (
     <>
       <div className="bg-blue-500 py-8 max-h-72">
--- src/contexts/budgetContext/BudgetContext.tsx
+++ src/contexts/budgetContext/BudgetContext.tsx
@@ -1,12 +1,18 @@
 import { useReducer, createContext } from "react";
 import { budgetReducer, initialState } from "../../reducers/budget-reducer";
 import { BudgetContextProps, BudgetProviderProps } from "./BudgetContextTypes";
+import { useMemo } from "react";
 
 //Tambien puede ser "{} as BudgetContextProps"
 export const BudgetContext = createContext<BudgetContextProps>(null!);
 
-export const BudgetProvider = ({children}:BudgetProviderProps) => {
+export const BudgetProvider = ({ children }: BudgetProviderProps) => {
   const [state, dispatch] = useReducer(budgetReducer, initialState);
+  // const totalExpenses = useMemo(
+  //   () => state.expenses.reduce((total, expense) => expense.amount + total, 0),
+  //   [state.expenses]
+  // );
+  // const remainingBudget = state.budget - totalExpenses;
   return (
     <BudgetContext.Provider value={{ state, dispatch }}>
       {children}
